05cb7e436142f47681d04d81d7e40a60cfcebb93
Alexander <caminante.msk@gmail.com>
2022-04-07 20:45:42 -0300

    home first edition finished

--- src/scrollTo.js
+++ src/scrollTo.js
@@ -2,22 +2,24 @@ import { elementScrollIntoView } from "seamless-scroll-polyfill";
 
 const scrollTo = () => {
 
-  const links = document.querySelectorAll('.header__item a')
+  const contactLink = document.querySelectorAll('.header__item')[5]
+  const discountLink = document.querySelector('.timer__title')
+  
+  const allLinks = []
+  allLinks.push(contactLink,discountLink)
 
-  links.forEach((element) => {
-    element.addEventListener('click', (event) => {
+  allLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
       event.preventDefault()
 
-      const id = element.getAttribute('href').substring(13)
-      
+      const id = link.getAttribute('href').substring(1)
       const target = document.getElementById(id)
-     
 
-      elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" });
-
+      elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
+      
     })
-
   })
+  
 }
 
 export default scrollTo;
