7f474d7b9e07b1692a90066635b4a3b8ad619b33
Alexander <caminante.msk@gmail.com>
2022-04-26 14:12:57 -0300

    bugs resolved / arrows experiment

--- src/components/header/headerWithScroll.js
+++ src/components/header/headerWithScroll.js
@@ -2,22 +2,22 @@ import { elementScrollIntoView } from "seamless-scroll-polyfill";
 import $ from 'jquery';
 
 const headerWithScroll = () => {
-
+ 
   const scrolling = () => {
     const contactLink = document.querySelector('#toContacts')
     const promotionLink = document.getElementById('toPromotion')
     const creationLink = document.getElementById('toCreation')
     const homeLink = document.getElementById('toHome')
 
-
+    const header = document.querySelector('.header')
     const promotionBtn = document.querySelector('.promotion__btn')
     const creationBtn = document.querySelector('.creation__btn')
     const discountLink = document.querySelector('.timer__title')
     
     const allLinks = []
-    allLinks.push(contactLink, discountLink, promotionLink, homeLink, promotionBtn, creationLink, creationBtn)
-    
-  
+
+    allLinks.push(contactLink, discountLink, promotionLink, promotionBtn, creationLink, creationBtn,homeLink)
+
     allLinks.forEach((link) => {
       link.addEventListener('click', (event) => {
         event.preventDefault()
@@ -25,55 +25,73 @@ const headerWithScroll = () => {
         const id = link.getAttribute('href').substring(1)
         const target = document.getElementById(id)
 
-        elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
-      })
-    })
+        if (event.target === homeLink) {
+
+          if (header.classList.contains('scroll')) {
+            elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
+            console.log('scrolled');
+          } 
+
+        } else {
+          elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
+          console.log('scrolled');
+        }
+      }) 
+    })   
+  }
+
+  const scrollingBurger = (clickedLink) => {
+      
+      console.log(clickedLink);
+      const id = clickedLink.getAttribute('href').substring(1)
+      const target = document.getElementById(id)
+
+      elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
+      console.log('scrollingBurger');
   }
   
  
     //scroll/full header
   $(window).scroll(function () {
     if ($(this).scrollTop() > 1) {
-      $(".header, .header__logo, .header__subtitle, .header__title, .header__nav, .header__nav_burger").addClass("scroll");
+      $(".header, .header__logo, .header__title, .header__nav, .header__nav_burger").addClass("scroll");
     }
     else {
-      $(".header, .header__logo, .header__subtitle, .header__title, .header__nav, .header__nav_burger").removeClass("scroll");
-    }
-    
+      $(".header, .header__logo, .header__title, .header__nav, .header__nav_burger").removeClass("scroll");
+    } 
   });
 
+  
+  if (visualViewport.width > 700) {
 
-  if (screen.width > 700) {
     console.log('>700');
-    scrolling()
+    scrolling() 
+
   } else {
+
     console.log('<700');
     $(".header__nav_burger").click(function () {
-    
+      console.log('click');
       //lock/unlock body
       $("body").toggleClass("lock"); 
       if ($(window).scrollTop() < 1) {
-        $(".header, .header__logo, .header__subtitle, .header__title, .header__nav, .header__nav_burger").toggleClass("scroll");
+        $(".header, .header__logo, .header__title, .header__nav, .header__nav_burger").toggleClass("scroll");
         $(".header__nav_burger, .header__nav, .header__list, .header__item").toggleClass("dropped");
-        $(".header__item").click(function () {
-          scrolling()
+        $(".header__item").click(function (event) {
           $(".header__nav_burger, .header__nav, .header__list, .header__item").removeClass("dropped");
           $("body").removeClass("lock");
+          scrollingBurger(event.target)
         });
       } else {
         $(".header__nav_burger, .header__nav, .header__list, .header__item").toggleClass("dropped");
-        $(".header__item").click(function () {
-          scrolling()
+        $(".header__item").click(function (event) {
           $(".header__nav_burger, .header__nav, .header__list, .header__item").removeClass("dropped");
           $("body").removeClass("lock");
+          scrollingBurger(event.target)
         });
       }
     });
-
   }
-  
-
-
 }
 
 export default headerWithScroll
