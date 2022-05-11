ab91c5126baa9adf6e0ba6f938303d75b5251356
Alexander <caminante.msk@gmail.com>
2022-05-10 21:27:48 -0300

    video with placeholder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/index.scss'
 
 import headerWithScroll from './components/header/headerWithScroll'
+import home from './components/home/home'
 import discount from './components/discount/discount'
 import portfolio from './components/portfolio/portfolio'
 // import ask from './components/ask/ask'
@@ -12,6 +13,7 @@ import yall from 'yall-js'
 
 document.addEventListener("DOMContentLoaded", yall);
 headerWithScroll()
+home()
 discount()
 portfolio()
 // ask()
