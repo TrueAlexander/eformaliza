d2863a16e9160b1c94b9d2944cd6bd1f3710ff26
Alexander <caminante.msk@gmail.com>
2022-05-15 00:23:33 -0300

    added videos en/es

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,15 +4,47 @@ const home = () => {
   window.addEventListener('DOMContentLoaded', () => {
 
     const video = document.querySelector('.home__video')
+    const homeLang = document.querySelector('.home')
+    
 
     video.addEventListener('click', () => {
       video.innerHTML = ''
-      video.insertAdjacentHTML('beforeend', 
+
+      const videos = [
+        {
+          lan: "pt", 
+          url: "https://www.youtube.com/embed/zy-XbdPf8yU"
+        },
+        {
+          lan: "es", 
+          url: "https://www.youtube.com/embed/MPajIs5rJqE"
+        },
+        {
+          lan: "en", 
+          url: "https://www.youtube.com/embed/uDldkuvjNbE"
+        },  
+      ]
+
+      let actualLink
+
+      if (homeLang.id === "pt") {  
+        actualLink = videos[0].url
+      } else if (homeLang.id == "en") {
+        actualLink = videos[2].url
+      } else {
+         actualLink = videos[1].url   
+      }
+
+      const insertVideo = (link) => {
+
+        video.insertAdjacentHTML('beforeend', 
         `
-          <iframe src="https://www.youtube.com/embed/p5FUE9UvYbs?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+          <iframe src="${link}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
           </iframe>
         `  
       )
+    }
+    insertVideo(actualLink)
     })
   })
 
