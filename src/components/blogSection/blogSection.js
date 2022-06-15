03cf013d62f3e8d1ebbea8c32cd86bfc3c421478
Alexander <caminante.msk@gmail.com>
2022-06-15 12:35:49 -0300

    post anchors debugged

--- src/components/blogSection/blogSection.js
+++ src/components/blogSection/blogSection.js
@@ -9,17 +9,20 @@ const blogSection = () => {
     const link3 = document.getElementById('link3')
     const link4 = document.getElementById('link4')
     const link5 = document.getElementById('link5')
-    const resumo = document.getElementById('resumo')
+    const resumo = document.getElementById('resumoLink')
     const voltar = document.getElementById('voltar')
     
     const allLinks = []
     allLinks.push(link1, link2, link3, link4, link5, resumo, voltar)
+
     
     allLinks.forEach((link) => {
       link.addEventListener('click', (event) => {
         event.preventDefault()
         const id = link.getAttribute('href').substring(1)
+        
         const target = document.getElementById(id)
+        console.log(target)
         elementScrollIntoView(target, { behavior: "smooth", block: "start" , inline: "center" })
       }) 
     })    
