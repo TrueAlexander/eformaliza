90b70fc443ad78f5b36b845f562cc69ec6aa9880
Alexander <caminante.msk@gmail.com>
2022-06-24 17:49:17 -0300

    second post

--- src/components/blogSection/blogSection.js
+++ src/components/blogSection/blogSection.js
@@ -11,9 +11,17 @@ const blogSection = () => {
     const link5 = document.getElementById('link5')
     const resumo = document.getElementById('resumoLink')
     const voltar = document.getElementById('voltar')
+    const voltar2 = document.getElementById('voltar2')
+
+    const link1_2 = document.getElementById('link1_2')
+    const link2_2 = document.getElementById('link2_2')
+    const link3_2 = document.getElementById('link3_2')
+    const link4_2 = document.getElementById('link4_2')
+    const resumo2 = document.getElementById('resumoLink_2')
+
     
     const allLinks = []
-    allLinks.push(link1, link2, link3, link4, link5, resumo, voltar)
+    allLinks.push(link1, link2, link3, link4, link5, resumo, voltar, voltar2, link1_2, link2_2, link3_2, link4_2, resumo2)
 
     
     allLinks.forEach((link) => {
